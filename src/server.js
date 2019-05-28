97efbb43995797e5dfe067e4a56129ff79fd464e
Dylan Miller <dylan.christopher.miller@gmail.com>
2019-05-28 13:01:48 -0700

    Use HTTPS for Prisma server

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ const resolvers = {
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   //endpoint: 'https://dfinity-explorer-p-54a68e4bba.herokuapp.com/dfinity-explorer-service-a/dev',
-  endpoint: 'http://prism-publi-hwk7lncrmzx3-1989461046.us-west-2.elb.amazonaws.com/prisma-service/dev',
+  endpoint: 'https://prisma.dfinityexplorer.org/prisma-service/dev',
   secret: process.env.PRISMA_SECRET,
   // Setting debug to true means that all requests made by Prisma binding instance to Prisma
   // API will be logged to the console. Set to false for production.
