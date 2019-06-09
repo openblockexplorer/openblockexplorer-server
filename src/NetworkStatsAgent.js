b9b770b39a4ebb8888ba6283c72c99cb9948f73d
Dylan Miller <dylan.christopher.miller@gmail.com>
2019-06-08 17:04:18 -0700

    Only update daily network stats once a day

--- src/NetworkStatsAgent.js
+++ src/NetworkStatsAgent.js
@@ -19,13 +19,7 @@ module.exports = class NetworkStatsAgent {
 
     this.blocks = [];
     this.numTransactions = 0;
-    this.dailyNetworkStats = {
-      date: this.getCurrentUTCDate(),
-      numBlocks: 0,
-      numTransactions: 0
-    };
-    this.startBlockHeight = 0;//!!!
-    this.lastBlockHeight = 0;//!!!
+    this.date = null;
   }
 
   /**
@@ -51,8 +45,8 @@ module.exports = class NetworkStatsAgent {
   }
 
   /**
-   * Handler for the block subscription, which adds a network stats object to the Prisma server for
-   * every block.
+   * Handler for the block subscription, which updates the network stats object on the Prisma server
+   * for every block.
    * @param {Object} subscription The subscription object.
    * @private
    */
@@ -84,7 +78,7 @@ module.exports = class NetworkStatsAgent {
         this.blocks.shift();
       }
 
-      // Add a new network stats object to the Prisma server.
+      // Update the network stats object on the Prisma server.
       if (this.blocks.length >= 2) {
         const numBlocks = this.blocks[this.blocks.length-1].height - this.blocks[0].height;
         const seconds = (this.blocks[this.blocks.length-1].timestamp - this.blocks[0].timestamp) / 1000;
@@ -115,100 +109,89 @@ module.exports = class NetworkStatsAgent {
           .catch(error => console.log(error));
       }
 
-      //!!!
-      if (this.lastBlockHeight === 0) {
-        this.startBlockHeight = block.height;
-        console.log(`NetworkStatsAgent started: startBlockHeight(${block.height}), this.lastBlockHeight(0)`);
+      // Process started, set this.date to yesterday to trigger a call to createDailyNetworkStats().
+      if (this.date === null) {
+        const yesterday = new Date();
+        yesterday.setDate(block.timestamp - 1);
+        this.date = this.getUTCDate(yesterday);
       }
-      else if (block.height !== this.lastBlockHeight + 1) {
-        // We also need to account for missed blocks due to the subscription not working!!!
-        // It can't all be done on the fly, we need to go back and verify a day after it's done.
-        console.log(`NetworkStatsAgent missed blocks: block.height(${block.height}) != lastBlockHeight(${this.lastBlockHeight}) + 1`);
-      }
-      this.lastBlockHeight = block.height;
 
-      // Process restarted, calculate daily network stats. The blocksConnection and
-      // transactionsConnection operations are slow, so change this code if we come up with a faster
-      // way to get the daily numBlocks and numTransactions.
-      // We should break this function apart into sub-functions!!!
-      // To make sure all daily network stats are valid, could have some kind of checkpoint/validated member in object!!!
-      // Whenever process starts, we could go through previous week and recalculate any completed days which are not yet checkpointed/validated!!!
-      if (this.dailyNetworkStats.numBlocks === 0) {
-        let connection = await this.prisma.query
-          .blocksConnection(
-            {
-              where: {
-                AND: [
-                  {timestamp_gt: this.dailyNetworkStats.date},
-                  {height_lt: block.height}
-                ]
-              }
-            },
-            '{ aggregate { count } }')
-          .catch(error => console.log(error));
-        this.dailyNetworkStats.numBlocks = connection.aggregate.count;
-        console.log(`blocksConnection blocks: ${this.dailyNetworkStats.numBlocks}`)
-        connection = await this.prisma.query
-          .transactionsConnection(
-            {
-              where: {
-                block: {
-                  AND: [
-                    {timestamp_gt: this.dailyNetworkStats.date},
-                    {height_lt: block.height}
-                  ]
-                }
-              }
-            },
-            '{ aggregate { count } }')
-          .catch(error => console.log(error));
-        this.dailyNetworkStats.numTransactions = connection.aggregate.count;
-        console.log(`transactionsConnection transactions: ${this.dailyNetworkStats.numTransactions}`)
-        this.startBlockHeight = block.height - this.dailyNetworkStats.numBlocks;//!!!
+      // If it is a new day, create/update the daily network stats object on the Prisma server.
+      const date = this.getUTCDate(block.timestamp);
+      if (date.getTime() !== this.date.getTime()) {
+        createDailyNetworkStats(this.date);
+        this.date = date;
       }
-      //!!!
-
-      // Create/update the daily network stats object on the Prisma server.
-      const date = this.getCurrentUTCDate();
-      if (date.getTime() !== this.dailyNetworkStats.date.getTime()) {
-        this.dailyNetworkStats.date = date;
-        this.dailyNetworkStats.numBlocks = 0;
-        this.dailyNetworkStats.numTransactions = 0;
-        this.startBlockHeight = block.height;//!!!
-        console.log(`NetworkStatsAgent new day: dailyNetworkStats reset`);//!!!
-      }
-      this.dailyNetworkStats.numBlocks++;
-      //!!!
-      if ((this.dailyNetworkStats.numBlocks % 100) == 0 ||
-        (this.numTransactions < 400 && 
-          (this.dailyNetworkStats.numBlocks  % 10) == 0)) {
-        const actualNumBlocks = block.height - this.startBlockHeight + 1;
-        console.log(
-          `NetworkStatsAgent: dailyNetworkStats.numBlocks: ${this.dailyNetworkStats.numBlocks}, actualNumBlocks: ${actualNumBlocks}`);
-      }
-      //!!!
-      this.dailyNetworkStats.numTransactions += block.numTransactions;
-      this.prisma.mutation
-        .upsertDailyNetworkStats(
-          {
-            where: { date: this.dailyNetworkStats.date },
-            create: this.dailyNetworkStats,
-            update: this.dailyNetworkStats
-          },
-          '{ date }'
-        )
-        .catch(error => console.log(error));
     }
     while (!result.done);
   }
 
   /**
-   * Returns the current UTC date with a time of 00:00:00.000 (midnight).
-   * @return {Object} the current UTC date with a time of 00:00:00.000 (midnight).
+   * Create a daily network stats object on the Prisma server for the specified date.
+   * @param {Object} date The date to create the daily network stats object for.
    * @private
    */
-  getCurrentUTCDate() {
-    const date = new Date();
+  async createDailyNetworkStats(date) {
+    const dateAfter = new Date();
+    dateAfter.setDate(date + 1);
+
+    // Calculate daily network stats.
+    let connection = await this.prisma.query
+      .blocksConnection(
+        {
+          where: {
+            AND: [
+              {timestamp_gte: date},
+              {timestamp_lt: dateAfter}
+            ]
+          }
+        },
+        '{ aggregate { count } }')
+      .catch(error => console.log(error));
+    const numBlocks = connection.aggregate.count;
+
+    connection = await this.prisma.query
+      .transactionsConnection(
+        {
+          where: {
+            block: {
+              AND: [
+                {timestamp_gte: date},
+                {timestamp_lt: dateAfter}
+              ]
+            }
+          }
+        },
+        '{ aggregate { count } }')
+      .catch(error => console.log(error));
+    const numTransactions = connection.aggregate.count;
+
+    const dailyNetworkStats = {
+      date,
+      numBlocks: numBlocks,
+      numTransactions: numTransactions
+    };
+
+    // Create/update the daily network stats object on the Prisma server.
+    this.prisma.mutation
+      .upsertDailyNetworkStats(
+        {
+          where: { date: date },
+          create: dailyNetworkStats,
+          update: dailyNetworkStats
+        },
+        '{ date }'
+      )
+      .catch(error => console.log(error));
+  }
+
+  /**
+   * Returns the UTC date of the specified date, but with a time of 00:00:00.000 (midnight).
+   * @param {Object} date The date to return the UTC date 00:00:00.000 of.
+   * @return {Object} The UTC date with a time of 00:00:00.000 (midnight).
+   * @private
+   */
+  getUTCDate(date) {
     return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()));
   }
 };
